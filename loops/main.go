cae423c4462aacc862458219c679f6a961fa5ca5
nikandfor <nikandfor@gmail.com>
2016-03-24 12:24:43 +0300

    loops

---
+++ loops/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	var a int = 2
+	//	b := 4
+
+	fmt.Printf("inf loop\n")
+	i := 0
+	for {
+		fmt.Printf("i1 : %v\n", i)
+		i++
+
+		if !(i < 3) {
+			break
+		}
+	}
+
+	fmt.Printf("2 loop\n")
+	i = 0
+	for i < 3 {
+		fmt.Printf("i2 : %v\n", i)
+		i++
+	}
+
+	fmt.Printf("3 loop\n")
+	for i = 0; i < 3; i++ {
+		fmt.Printf("i3 : %v\n", i)
+	}
+}
